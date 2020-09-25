7fc4ddd1558e4d427744204f4beba0a68aff2548
kanav <kanav@DESKTOP-NLK35I9.home>
2020-09-25 11:38:46 -0400

    operations wkst

---
+++ github.kanav/src/github/kanav/operations_wkst.java
@@ -0,0 +1,24 @@
+package github.kanav;
+
+public class operations_wkst {
+
+
+	
+	public void intToChar(int x) {
+		
+		System.out.println((char)x);
+	}
+		
+	public void lastDigit(int x) {
+		
+		System.out.println(x % 10);
+		
+	}
+
+	public void average(double x, double y) {
+		
+		System.out.println((x+y/2));
+		
+	}
+	
+}
