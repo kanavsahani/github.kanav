8ae70a9cdb5b89972531b5aec1ab5dcb7f2fe614
kanav <kanav@10.1.68.44>
2021-09-14 13:53:03 -0400

    homework

---
+++ github.kanav/src/github/kanav/Intro.java
@@ -0,0 +1,10 @@
+package github.kanav;
+
+public class Intro {
+	public static void main(String[] args) {
+		System.out.println("What is your name? My name is Kanav Sahani");
+		System.out.println("Where are you from? I am from Sudbury Massachusetts");
+		System.out.println("What form are you in? I am a fifth former");
+		System.out.println("How was your summer? It was great! I went to New Orleans and New Hampshire");
+	}
+}
