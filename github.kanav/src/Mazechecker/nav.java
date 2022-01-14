b5b89d96eb1687612cd9877aa3fa7cb2627428e1
kanav <kanav@10.1.232.91>
2022-01-14 12:56:20 -0500

    new bot. this works!

--- github.kanav/src/Mazechecker/nav.java
+++ github.kanav/src/Mazechecker/nav.java
@@ -1,6 +1,7 @@
 package Mazechecker;
 
 import java.awt.Color;
+import java.util.Random;
 
 public class nav extends Bot{
 String mode = "forward";
@@ -8,23 +9,20 @@ String mode = "forward";
 int startX, startY; // Starting X and Y values of maze
 int endX, endY;     // Ending X and Y values of maze
 
-	
+	boolean pen = false;
+	Random random;
 	public nav(MazeRunner mr, Color c) {
 		super(mr,c);
+		random = new Random();
 		
 	}
 
 	public void move() {
-
-		if (mode.equals("forward")) {
+		pen = random.nextBoolean();
+		if(pen)
 			moveForward();
-			mode = "left";
-			
-		}		
-		else {
+		else
 			turnLeft();
-			mode = "forward";
-		}
 	}
 }
 
