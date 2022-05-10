03e8a0b3b86409d4e522a9565db7bec0053a0375
kanav <kanavsahani@stmarksschool.org>
2022-05-09 20:12:13 -0400

    Dijkstras/GPS

--- github.kanav/src/Dijkstras/Dijkstras.java
+++ github.kanav/src/Dijkstras/Dijkstras.java
@@ -2,13 +2,8 @@ package Dijkstras;
 
 public class Dijkstras {
 	public Dijkstras() {
-		Distancegraph<String, Integer> graph = new Distancegraph();
+		Distancegraph graph = new Distancegraph();
 		String a = "A", b = "B", c = "C", d = "D", e = "E";
-        graph.add(a);
-        graph.add(b);
-        graph.add(c);
-        graph.add(d);
-        graph.add(e);
         graph.connect(a, b, 2);
         graph.connect(b, c, 5);
         graph.connect(a, e, 1);
